39f12b38bf449a9f3a99dc736d80c1ce48913d4c
oonbeh <oonbeh@gmail.com>
2022-03-02 21:34:25 +0800

    Add files via upload

---
+++ control-remove.js
@@ -0,0 +1,32 @@
+const dataRemove = [
+  {
+    "target":"brainly.co.id",
+    "selector":[
+      ".brn-placeholder__animation-box",
+      ".sg-space-ignore",
+      "header",
+      ".brn-placeholder__animation",
+      ".brn-header-container",
+      ".brn-ads-mobile-container",
+      ".brn-ads-box",
+      ".brn-footer__select"
+    ]
+  }
+];
+
+(()=>{
+  for(let aa of dataRemove){
+    let tAA = aa.target;
+    if(window.location.href.indexOf(tAA)>0){
+      let sAA = aa.selector;
+      for(let bb of sAA){
+        let getSelect = document.querySelectorAll(bb);
+        for(let cc of getSelect){
+          if(cc){
+            cc.remove();
+          };
+        };
+      };
+    };
+  };
+})();
